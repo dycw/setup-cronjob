9bd24cde76ca4b11bb6b17903bacfec8a24da7a8
Derek Wan <d.wan@icloud.com>
2025-12-20 11:10:16 +0900

    2025-12-20 11:10:16 (Sat)  > DW-Mac  > derekwan

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -12,3 +12,12 @@ PATH=/usr/local/bin:/usr/bin:/bin
 * * * * * nonroot (echo "[$(date '+\\%Y-\\%m-\\%d \\%H:\\%M:\\%S') | $$] Starting 'name'..."; flock --nonblock --verbose /tmp/cron-name.lock timeout --kill-after=10s --verbose 60s script.py; echo "[$(date '+\\%Y-\\%m-\\%d \\%H:\\%M:\\%S') | $$] Finished 'name' with exit code $?") 2>&1 | sudo tee -a /var/log/name.log
 """
         assert result == expected
+
+    def test_prepend_path(self) -> None:
+        result = _get_crontab()
+        expected = """\
+PATH=/usr/local/bin:/usr/bin:/bin
+
+* * * * * nonroot (echo "[$(date '+\\%Y-\\%m-\\%d \\%H:\\%M:\\%S') | $$] Starting 'name'..."; flock --nonblock --verbose /tmp/cron-name.lock timeout --kill-after=10s --verbose 60s script.py; echo "[$(date '+\\%Y-\\%m-\\%d \\%H:\\%M:\\%S') | $$] Finished 'name' with exit code $?") 2>&1 | sudo tee -a /var/log/name.log
+"""
+        assert result == expected
